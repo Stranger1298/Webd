6e8a00817d78a512f3d306de8e4bcd8b92c1545c
Aman <amanraj89969@gmail.com>
2023-08-23 23:35:58 +0530

    comit

--- Code/Solana/sol_codes/src/main.rs
+++ Code/Solana/sol_codes/src/main.rs
@@ -1,7 +1,14 @@
+// fn main() {
+//     //string slicing
+//     let s=String::from("Hello duniya");
+//     //for printing hello
+//     let hello=&s[0..5];
+//     println!("{hello}");   
+// }
+use std::io;
 fn main() {
-    //string slicing
-    let s=String::from("Hello duniya");
-    //for printing hello
-    let hello=&s[0..5];
-    println!("{hello}");   
+    println!("Enter a number: ");
+    let mut a=String::new();
+    io::stdin().read_line(&mut a);
+    println!("{a}");
 }
